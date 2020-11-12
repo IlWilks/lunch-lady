e1a5e5024090e88706fa79c5ebde42e5321f593a
Ian <ianlloydwilkinson@gmail.com>
2020-11-12 14:33:55 -0700

    added hashes with entrees and sides

---
+++ app.rb
@@ -0,0 +1,12 @@
+entree_choices = {
+  message: "Welcome to lunch, please select an entree:",
+  options: ["Pizza", "Burrito", "Hamburger"],
+} 
+
+
+side_choices = {
+  message: "Please select 2 side choices:",
+  options: ["Mac and cheese", "Rice", "Corn", "tater tots"],
+}
+
+puts entree_choices
---
+++ menu.rb
@@ -0,0 +1,5 @@
+# create interface that displays main dishes, then prompts user to choose one
+#then lists side dishes and prompts user to choose 2
+#computer repeats users choices
+#total's lunch items and outputs prices
+
